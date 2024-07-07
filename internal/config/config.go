83d8bf8274f5f706990a715a2936cac5b931f272
Aleksei Rusinov <alxrusinov@yandex.ru>
2024-07-07 15:37:48 +0200

    iter5: completed

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,14 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
+
+const (
+	DeafaultBaseURL     = "localhost:8080"
+	DeafaultResponseURL = "http://localhost:8080"
+)
 
 type Config struct {
 	BaseURL     string
@@ -8,13 +16,21 @@ type Config struct {
 }
 
 func (config *Config) Init() {
-	flag.StringVar(&config.BaseURL, "a", "localhost:8080", "base url when server will be started")
-	flag.StringVar(&config.ResponseURL, "b", "http://localhost:8080", "base url of returning link")
+	flag.StringVar(&config.BaseURL, "a", DeafaultBaseURL, "base url when server will be started")
+	flag.StringVar(&config.ResponseURL, "b", DeafaultResponseURL, "base url of returning link")
 
 }
 
 func (config *Config) Parse() {
 	flag.Parse()
+
+	if baseURL, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+		config.BaseURL = baseURL
+	}
+
+	if responseURL, ok := os.LookupEnv("BASE_URL"); ok {
+		config.ResponseURL = responseURL
+	}
 }
 
 func CreateConfig() *Config {
