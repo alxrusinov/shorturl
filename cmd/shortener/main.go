4cefda3daba96715ce952425445beea0f7df0589
Aleksei Rusinov <alxrusinov@yandex.ru>
2024-11-09 19:30:55 +0200

    global vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/alxrusinov/shorturl/internal/app"
 	"github.com/alxrusinov/shorturl/internal/config"
 )
 
+var buildVersion string = "N/A"
+var buildDate string = "N/A"
+var buildCommit string = "N/A"
+
 func main() {
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+
 	config := config.NewConfig()
 
 	config.Init()
